0a42d7e5e3916eba2c78c527d4ef363c55ba1ca5
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-03-20 23:33:50 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -2,11 +2,6 @@ class Average
 {
     public static void main(String[] args)
     {
-        System.out.println("Name : Rachit Singhal");
-        System.out.println("Roll no : 41");
-        System.out.println("Section : B");
-        System.out.println("Course : MCA");
-        System.out.println("Semester : 2nd");
         int sum=0;
         for(int i=0;i<args.length;i++)
         {
