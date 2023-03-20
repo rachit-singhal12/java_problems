37bf5a4cf2e9d79c9a9b59f5cb9d67a082ce9afb
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-03-20 23:35:24 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -18,11 +18,6 @@ class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Name : Rachit Singhal");
-        System.out.println("Roll no : 41");
-        System.out.println("Section : B");
-        System.out.println("Course : MCA");
-        System.out.println("Semester : 2nd");
         Student obj = new Student();
         Scanner scan = new Scanner(System.in);
         String name;
