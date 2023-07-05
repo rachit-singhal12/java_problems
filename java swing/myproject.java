86ca2a8167a63d77cf73a0462405b93f36cc4eeb
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-05 18:14:17 +0530

    Update myproject.java

--- java swing/myproject.java
+++ java swing/myproject.java
@@ -5,7 +5,7 @@ public class myproject {
 	public static void main(String[] args)throws Exception {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mca","root","R@chit100");  
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mca","root","");  
 		Statement st = con.createStatement() ;
 		String query = "insert into mca values (102,'Akshay',45)";
 		st.executeUpdate(query);
